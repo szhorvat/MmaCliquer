a5082240c44d71c9b19c9e824472763b4db155fd
Szabolcs Horvát <szhorvat@gmail.com>
2015-06-15 15:04:02 +0200

    Separate function for maximal clique distribution

--- Cliquer.m
+++ Cliquer.m
@@ -14,7 +14,8 @@
 
 BeginPackage["Cliquer`", {"CCompilerDriver`"}]
 
-CliquerDistribution::usage = "CliquerDistribution[]"
+CliquerDistribution::usage = "CliquerDistribution[]";
+CliquerMaximalDistribution::usage = "CliquerMaximalDistribution[]";
 RecompileCliquer::usage = "RecompileCliquer[] recompiles the Cliquer library.";
 
 Begin["`Private`"] (* Begin Private Context *)
@@ -51,6 +52,8 @@ RecompileCliquer[opt:OptionsPattern[]] :=
   ]
 
 
+(* All cliques *)
+
 CliquerDistribution[g_?GraphQ, All|Infinity] := CliquerDistribution[g]
 
 CliquerDistribution[g_?GraphQ] :=
@@ -65,6 +68,22 @@ CliquerDistribution[g_?GraphQ, min_Integer?Positive, max_Integer?Positive] /; ma
       iCliquerDistribution[VertexCount[g], min, max, 0, List@@@EdgeList[g] - 1]
     ]
 
+
+(* Maximal cliques *)
+
+CliquerMaximalDistribution[g_?GraphQ, All|Infinity] := CliquerMaximalDistribution[g]
+
+CliquerMaximalDistribution[g_?GraphQ] :=
+    With[{res=CliquerMaximalDistribution[g, 1, VertexCount[g]]},
+      Drop[res, -LengthWhile[Reverse[res], #==0&]]
+    ]
+
+CliquerMaximalDistribution[g_?GraphQ, max_Integer?Positive] := CliquerMaximalDistribution[g, 1, max]
+
+CliquerMaximalDistribution[g_?GraphQ, min_Integer?Positive, max_Integer?Positive] /; max > min :=
+    Module[{},
+      iCliquerDistribution[VertexCount[g], min, max, 1, List@@@EdgeList[g] - 1]
+    ]
 
 (* Compile the Cliquer library if needed, then load functions *)
 If[FindLibrary["cliquer"] === $Failed,
