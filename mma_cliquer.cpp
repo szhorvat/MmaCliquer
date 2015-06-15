3ec551d7e5fefb44d49e20bb02d65bbcfa9dd5d2
Szabolcs Horvát <szhorvat@gmail.com>
2015-06-15 15:03:30 +0200

    Whitespace cleanup in cpp

--- mma_cliquer.cpp
+++ mma_cliquer.cpp
@@ -26,11 +26,11 @@ extern "C" DLLEXPORT int WolframLibrary_initialize(WolframLibraryData libData) {
 extern "C" DLLEXPORT void WolframLibrary_uninitialize(WolframLibraryData libData) { }
 
 extern "C" DLLEXPORT int m_clique_distribution(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
-	int n   = MArgument_getInteger(Args[0]);       // number of vertices
-	int min = MArgument_getInteger(Args[1]);       // minimum clique size
-	int max = MArgument_getInteger(Args[2]);       // maximum clique size
+	int n   = MArgument_getInteger(Args[0]);	   // number of vertices
+	int min = MArgument_getInteger(Args[1]);	   // minimum clique size
+	int max = MArgument_getInteger(Args[2]);	   // maximum clique size
 
-    boolean maximal_only = MArgument_getInteger(Args[3]); // find maximal cliques
+	boolean maximal_only = MArgument_getInteger(Args[3]); // find maximal cliques
 
 	MTensor edges = MArgument_getMTensor(Args[4]); // edge list as integer matrix
 	mint const *dims = libData->MTensor_getDimensions(edges);
@@ -46,8 +46,8 @@ extern "C" DLLEXPORT int m_clique_distribution(WolframLibraryData libData, mint 
 		GRAPH_ADD_EDGE(g, edgesData[2*i], edgesData[2*i+1]);
 	}
 
-    distribution = new long[max];
-    std::fill(distribution, distribution + max, 0);
+	distribution = new long[max];
+	std::fill(distribution, distribution + max, 0);
 
 	clique_default_options->user_function = &collector;
 	clique_unweighted_find_all(g, min, max, maximal_only, NULL);
@@ -59,8 +59,8 @@ extern "C" DLLEXPORT int m_clique_distribution(WolframLibraryData libData, mint 
 	MArgument_setMTensor(Res, result);
 
 	// clean up
-    delete [] distribution;
-    distribution = NULL;
+	delete [] distribution;
+	distribution = NULL;
 	graph_free(g);
 
 	return LIBRARY_NO_ERROR;
